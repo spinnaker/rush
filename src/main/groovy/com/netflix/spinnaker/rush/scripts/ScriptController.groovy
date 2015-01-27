b363a735accc949756875359105a1ff8245cab1e
duftler <duftler@google.com>
2015-01-27 12:59:22 -0500

    Define new taskLogs/{id} entrypoint. Returns log contents for specified task.

--- src/main/groovy/com/netflix/spinnaker/rush/scripts/ScriptController.groovy
+++ src/main/groovy/com/netflix/spinnaker/rush/scripts/ScriptController.groovy
@@ -52,4 +52,15 @@ class ScriptController {
     repo.get(id)
   }
 
+  @RequestMapping(value = 'taskLogs/{id}', method = RequestMethod.POST)
+  Map getLogs(@PathVariable(value='id') String id, @RequestBody @Valid ScriptConfig config) {
+    ScriptExecution scriptExecution = repo.get(id)
+
+    if (scriptExecution?.container) {
+      [logsContent: executor.getLogs(scriptExecution.container, config)]
+    } else {
+      null
+    }
+  }
+
 }
